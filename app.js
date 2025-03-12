f1fb31a6d816e9f2436c5584dac5780471319619
Hector Banda <bandahectorkh7@gmail.com>
2025-03-12 15:46:56 -0600

    Actualiza el archivo js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const inputAmigo = document.getElementById("amigo");
+const listaAmigos = [];
+const ulListaAmigos = document.getElementById("listaAmigos");
+
+function agregarAmigo(){
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}<li>`;
+
+}
